50bcdcbe0ebb8d2c7bcca949845484ed4df0eb47
bexzodxamdamov <bexzodxamdamov5@gmail.com>
2022-10-23 15:02:19 +0500

    first commit

---
+++ amaliyot/main.js
@@ -0,0 +1,20 @@
+// task 1
+// function da arifmetik amalla
+
+let one = Number(prompt("1st"));
+let two = Number(prompt("2st"));
+
+function plus(params) {
+  return one + two;
+}
+function minus(params) {
+  return one - two;
+}
+function multiplication(params) {
+  return one * two;
+}
+function division(params) {
+  return one / two;
+}
+
+console.log(multiplication());
